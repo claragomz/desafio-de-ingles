498a7ffb9e402e02cad088f3305f98d4e6cdce14
Ana Clara <gomesclara99x@gmail.com>
2022-07-12 15:15:07 -0300

    ok

---
+++ index.js
@@ -0,0 +1,17 @@
+let comedia = [{
+    title: 'As Branquelas',
+    diretor: 'Keenen Ivory Wayans',
+    year: 2004
+}]
+
+let action = [{
+    title: 'Duro de Matar',
+    diretor:'John McTiernan',
+    year:1989
+}]
+
+let fantasy = [{
+    title: 'Alice no Pais das Maravilhas',
+    diretor: 'Tim Burton',
+    year: 2010
+}] 
